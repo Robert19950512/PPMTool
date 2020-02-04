4f6cf0cece259f5934c88486d2211b684b699721
Fa Long <f.long@wustl.edu>
2020-02-04 01:01:33 -0600

    update task form part2

--- ppmtool-react-client/src/actions/backlogActions.js
+++ ppmtool-react-client/src/actions/backlogActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ERRORS, GET_BACKLOG } from "./types.js";
+import { GET_ERRORS, GET_BACKLOG, GET_PROJECT_TASK } from "./types.js";
 
 export const addProjectTask = (
   backlog_id,
@@ -28,5 +28,26 @@ export const getBacklog = backlog_id => async dispatch => {
       type: GET_BACKLOG,
       payload: res.data
     });
-  } catch (error) {}
+  } catch (err) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    });
+  }
+};
+
+export const getProjectTask = (
+  backlog_id,
+  pt_id,
+  history
+) => async dispatch => {
+  try {
+    const res = await axios.get(`/api/backlog/${backlog_id}/${pt_id}`);
+    dispatch({
+      type: GET_PROJECT_TASK,
+      payload: res.data
+    });
+  } catch (error) {
+    history.push("/dashboard");
+  }
 };
--- ppmtool-react-client/src/components/ProjectBoard/ProjectBoard.js
+++ ppmtool-react-client/src/components/ProjectBoard/ProjectBoard.js
@@ -7,13 +7,50 @@ import { getBacklog } from "../../actions/backlogActions";
 
 class ProjectBoard extends Component {
   // constructor to handle errors
+  constructor() {
+    super();
+    this.state = {
+      errors: {}
+    };
+  }
   componentDidMount() {
     const { id } = this.props.match.params;
     this.props.getBacklog(id);
   }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
   render() {
     const { id } = this.props.match.params;
     const { project_tasks } = this.props.backlog;
+    const { errors } = this.state;
+    let BoardContent;
+
+    const boardAlgorithm = (errors, project_tasks) => {
+      if (project_tasks.length < 1) {
+        if (errors.projectNotFound) {
+          return (
+            <div className="alert alert-danger text-center " role="alert">
+              {errors.projectNotFound}
+            </div>
+          );
+        } else {
+          return (
+            <div className="alert alert-info text-center" role="alert">
+              No Project Tasks on this board
+            </div>
+          );
+        }
+      } else {
+        return <Backlog project_tasks_props={project_tasks} />;
+      }
+    };
+
+    BoardContent = boardAlgorithm(errors, project_tasks);
 
     return (
       <div className="container">
@@ -22,10 +59,10 @@ class ProjectBoard extends Component {
         </Link>
         <br />
         <hr />
+        {BoardContent}
         {
           //<!-- Backlog STARTS HERE -->
         }
-        <Backlog project_tasks_props={project_tasks} />
       </div>
     );
   }
@@ -33,11 +70,13 @@ class ProjectBoard extends Component {
 
 ProjectBoard.propTypes = {
   backlog: PropTypes.object.isRequired,
-  getBacklog: PropTypes.func.isRequired
+  getBacklog: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-  backlog: state.backlog
+  backlog: state.backlog,
+  errors: state.errors
 });
 
 export default connect(mapStateToProps, { getBacklog })(ProjectBoard);
