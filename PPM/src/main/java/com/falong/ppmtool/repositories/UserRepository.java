e823fc8f27c1e145eb2f1ae6480508d9979d67ca
Fa Long <f.long@wustl.edu>
2020-02-10 22:44:39 -0600

    compare1

---
+++ PPM/src/main/java/com/falong/ppmtool/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.falong.ppmtool.repositories;
+
+import com.falong.ppmtool.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
---
+++ PPM/src/main/java/com/falong/ppmtool/services/UserService.java
@@ -0,0 +1,29 @@
+package com.falong.ppmtool.services;
+
+import com.falong.ppmtool.domain.User;
+import com.falong.ppmtool.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public User saveUser(User newUser) {
+        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+        // username has to be unique(exception)
+        // make sure password and confirmPassword match
+        // we don't show or persist the confirmPassword
+        return userRepository.save(newUser);
+//        try{
+//
+//        }catch{
+//
+//        }
+    }
+}
